63a9558e5141df3f2a9108eaf2dfc3d1f138f60c
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-09-06 22:11:16 +0700

    init VehicleModel

---
+++ Application.Web.Database/Models/VehicleImage.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Web.Database.Models
+{
+    public class VehicleImage
+    {
+        public Guid VehicleId { get; set; }
+        public Guid ImageId { get; set; }
+
+        public virtual Vehicle Vehicle { get; set; }
+
+        public virtual Image Image { get; set; }
+    }
+}
--- Application.Web.Service/Services/ProductService.cs
+++ Application.Web.Service/Services/ProductService.cs
@@ -1,12 +1,19 @@
-﻿using Application.Web.Service.Interfaces;
+﻿using Application.Web.Database.Models;
+using Application.Web.Database.Repository;
+using Application.Web.Database.UnitOfWork;
+using Application.Web.Service.Interfaces;
 
 namespace Application.Web.Service.Services
 {
     public class ProductService : IProductService
     {
-        public ProductService()
-        {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<Vehicle> _productRepo;
 
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _productRepo = unitOfWork.GetBaseRepo<Vehicle>();
         }
     }
 }
