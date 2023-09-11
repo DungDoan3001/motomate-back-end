34df4cb67c0f41b7f2524115da5cbcd7ba3a9dac
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-09-11 22:20:15 +0200

    update namespace

--- Application.Web.Service/Services/VehicleService.cs
+++ Application.Web.Service/Services/VehicleService.cs
@@ -5,12 +5,12 @@ using Application.Web.Service.Interfaces;
 
 namespace Application.Web.Service.Services
 {
-    public class ProductService : IProductService
+    public class VehicleService : IVehicleService
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IGenericRepository<Vehicle> _productRepo;
 
-        public ProductService(IUnitOfWork unitOfWork)
+        public VehicleService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
             _productRepo = unitOfWork.GetBaseRepo<Vehicle>();
--- Applicaton.Web.API/Controllers/VehicleController.cs
+++ Applicaton.Web.API/Controllers/VehicleController.cs
@@ -5,14 +5,14 @@ namespace Applicaton.Web.API.Controllers
 {
     [Route("api/product")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class VehicleController : ControllerBase
     {
-        private readonly ILogger<ProductController> _logger;
+        private readonly ILogger<VehicleController> _logger;
         private readonly IMapper _mapper;
         private const string controllerPrefix = "Product";
         private const int maxPageSize = 20;
 
-        public ProductController(ILogger<ProductController> logger, IMapper mapper)
+        public VehicleController(ILogger<VehicleController> logger, IMapper mapper)
         {
             _logger = logger;
             _mapper = mapper;
