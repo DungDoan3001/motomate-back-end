6d715d5d52bafe1bf50f36e22ae4c1d7a665fb98
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-09-25 13:30:58 +0700

    update filtering

--- Application.Web.Database/Migrations/20230925052011_addCityFieldToVehicle2.Designer.cs
+++ Application.Web.Database/Migrations/20230925052011_addCityFieldToVehicle2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Application.Web.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Application.Web.Database.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230925052011_addCityFieldToVehicle2")]
+    partial class addCityFieldToVehicle2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -221,14 +223,14 @@ namespace Application.Web.Database.Migrations
                         new
                         {
                             Id = new Guid("60929087-1227-4efd-af43-e9ae2524eb0e"),
-                            ConcurrencyStamp = "21f7325c-fbe9-423f-9660-2425c1f07c30",
+                            ConcurrencyStamp = "2ad79953-2618-4bbc-86a3-5e017dbfe1c3",
                             Name = "Admin",
                             NormalizedName = "ADMIN"
                         },
                         new
                         {
                             Id = new Guid("7e8e25ca-fd0a-4271-b7e9-fe61ffcff2c1"),
-                            ConcurrencyStamp = "1bcfc9cb-da90-4a0e-9a3f-d8f94929e1a5",
+                            ConcurrencyStamp = "114d7488-8436-4e64-9553-2e7ae9205e22",
                             Name = "User",
                             NormalizedName = "USER"
                         });
