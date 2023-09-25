3f26e37ef23684ee28fef2da97f29ada71dfd237
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-09-25 09:42:58 +0700

    add Username field to user respone

--- Application.Web.Database/DTOs/ResponseModels/UserResponseModel.cs
+++ Application.Web.Database/DTOs/ResponseModels/UserResponseModel.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
