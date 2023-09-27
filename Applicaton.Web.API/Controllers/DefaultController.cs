e7b192ba34b99c1a6034d1f257662e8bc4380ef2
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-09-28 00:30:26 +0700

    Enhance: Auto redirect to swagger

---
+++ Applicaton.Web.API/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Applicaton.Web.API.Controllers
+{
+	[Route("")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+        public DefaultController()
+        {
+            
+        }
+
+		[Route(""), HttpGet]
+		[ApiExplorerSettings(IgnoreApi = true)]
+		public RedirectResult RedirectToSwaggerUi()
+		{
+			return Redirect("/swagger/");
+		}
+	}
+}
