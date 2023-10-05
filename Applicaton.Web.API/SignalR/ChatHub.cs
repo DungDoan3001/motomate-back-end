b531996ee92c8ba938ebbb39c826d9e138eca721
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-10-05 23:31:08 +0700

    UD first signalR

--- Applicaton.Web.API/SignalR/ChatHub.cs
+++ Applicaton.Web.API/SignalR/ChatHub.cs
@@ -1,20 +1,54 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Application.Web.Database.DTOs.RequestModels;
+using Application.Web.Database.DTOs.ResponseModels;
+using Application.Web.Service.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Applicaton.Web.API.SignalR
 {
 	public class ChatHub : Hub
 	{
-		public ChatHub()
-		{
+		private readonly IChatService _chatService;
+		private readonly IMapper _mapper;
 
+		public ChatHub(IMapper mapper, IChatService chatService)
+		{
+			_chatService = chatService;
+			_mapper = mapper;	
 		}
 
-		//public override async Task OnConnectedAsync()
-		//{
-		//	var httpContext = Context.GetHttpContext();
-		//	var activityId = httpContext.Request.Query["activityId"];
-		//	await Groups.AddToGroupAsync(Context.ConnectionId, activityId);
-		//	var result = await
-		//}
+		public async Task CreateChatAsync(ChatRequestModel requestModel)
+		{
+			var chat = await _chatService.CreateChatAsync(requestModel);
+
+			var chatDto = _mapper.Map<ChatResponseModel>(chat);
+
+			foreach (var item in chat.ChatMembers)
+			{
+				await Clients.Group(item.UserId.ToString()).SendAsync("ReceiveChat", chatDto);
+			};
+		}
+
+		public override async Task OnConnectedAsync()
+		{
+			var httpContext = Context.GetHttpContext();
+			var userId = httpContext.Request.Query["userId"];
+			var pageNumber = int.Parse(httpContext.Request.Query["pageNumber"]);
+			var pageSize = int.Parse(httpContext.Request.Query["pageSize"]);
+
+			var paginationData = new PaginationRequestModel
+			{
+				pageNumber = pageNumber,
+				pageSize = pageSize
+			};
+
+			await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+
+			var chats = await _chatService.GetAllChatsByUserAsync(paginationData, Guid.Parse(userId));
+
+			var chatDtos = _mapper.Map<IEnumerable<ChatResponseModel>>(chats);
+
+			await Clients.Caller.SendAsync("LoadChats", chatDtos);
+		}
 	}
 }
