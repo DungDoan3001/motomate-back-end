c94baa8ef626099bb873d325f2f58d0729321792
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-10-26 22:22:01 +0700

    mapping profiles

---
+++ Application.Web.Database/DTOs/RequestModels/BlogRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Web.Database.DTOs.RequestModels
+{
+	public class BlogRequestModel
+	{
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public string ShortDescription { get; set; }
+		public string ImageUrl { get; set; }
+		public string PublicId { get; set; }
+		public Guid AuthorId { get; set; }
+		public Guid CategoryId { get; set; }
+	}
+}
---
+++ Application.Web.Database/DTOs/ResponseModels/BlogResponseModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Application.Web.Database.DTOs.ResponseModels
+{
+	public class BlogResponseModel
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public string ShortDescription { get; set; }
+		public CategoryOfBlog Category { get; set; }
+		public AuthorOfBlog Author { get; set; }
+		public ImageOfBlog Image { get; set; }
+	}
+
+	public class ImageOfBlog
+	{
+		public string ImageUrl { get; set; }
+		public string PublicId { get; set; }
+	}
+
+	public class AuthorOfBlog
+	{
+		public Guid AuthorId { get; set; }
+		public string Username { get; set; }
+		public string Picture { get; set; }
+	}
+
+	public class CategoryOfBlog
+	{
+		public Guid CategoryId { get; set; }
+		public string Name { get; set; }
+	}
+}
