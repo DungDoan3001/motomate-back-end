10f264294318d4cfd47b9b7e1a52983c9a813a1c
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-10-26 22:26:19 +0700

    base blog service

---
+++ Application.Web.Database/Queries/ServiceQueries/BlogQueries.cs
@@ -0,0 +1,11 @@
+﻿using Application.Web.Database.Context;
+using Application.Web.Database.Models;
+using Application.Web.Database.Queries.Interface;
+
+namespace Application.Web.Database.Queries.ServiceQueries
+{
+	public class BlogQueries : BaseQuery<Blog>, IBlogQueries
+	{
+		public BlogQueries(ApplicationContext dbContext) : base(dbContext) { }
+	}
+}
---
+++ Application.Web.Service/Services/BlogService.cs
@@ -0,0 +1,22 @@
+﻿using Application.Web.Database.Models;
+using Application.Web.Database.Queries.Interface;
+using Application.Web.Database.Repository;
+using Application.Web.Database.UnitOfWork;
+using Application.Web.Service.Interfaces;
+
+namespace Application.Web.Service.Services
+{
+	public class BlogService : IBlogService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IGenericRepository<Blog> _blogRepo;
+		private readonly IBlogQueries _blogQueries;
+
+		public BlogService(IUnitOfWork unitOfWork, IBlogQueries blogQueries)
+		{
+			_unitOfWork = unitOfWork;
+			_blogRepo = unitOfWork.GetBaseRepo<Blog>();
+			_blogQueries = blogQueries;
+		}
+	}
+}
