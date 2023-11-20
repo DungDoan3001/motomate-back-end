7d7a0edab44962d89c58ed96cff4f3df20eb445d
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-11-20 14:13:11 +0700

    add new roles

--- Application.Web.Database/Constants/SeedDatabaseConstant.cs
+++ Application.Web.Database/Constants/SeedDatabaseConstant.cs
@@ -18,9 +18,23 @@ namespace Application.Web.Database.Constants
             NormalizedName = "USER"
         };
 
-        public static List<Role> DEFAULT_ROLES = new List<Role>
+        public static Role LESSOR = new Role
         {
-            ADMIN, USER
+            Id = Guid.Parse("20308981-486a-456e-833c-1a8821121b3e"),
+            Name = "Lessor",
+            NormalizedName = "LESSOR"
+        };
+
+		public static Role STAFF = new Role
+		{
+			Id = Guid.Parse("19021360-14c6-49ab-a91b-79c388115f4e"),
+			Name = "Staff",
+			NormalizedName = "STAFF"
+		};
+
+		public static List<Role> DEFAULT_ROLES = new List<Role>
+        {
+            ADMIN, USER, LESSOR, STAFF
         };
     }
 }
