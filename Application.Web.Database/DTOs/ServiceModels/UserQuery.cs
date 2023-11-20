aa68965ea4573c91f32c22abe83a8533e8c9c3cf
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-11-20 17:40:20 +0700

    update

--- Application.Web.Database/DTOs/ServiceModels/UserQuery.cs
+++ Application.Web.Database/DTOs/ServiceModels/UserQuery.cs
@@ -7,9 +7,6 @@ namespace Application.Web.Database.DTOs.ServiceModels
 		[JsonPropertyName("roles")]
 		public List<string> Roles {  get; set; }
 
-		[JsonPropertyName("cities")]
-		public List<string> Cities { get; set; }
-
 		[JsonPropertyName("query")]
 		public string Query { get; set; }
 	}
