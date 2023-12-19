3d7aa222c46081b66c6616e6cc783dadc21ced6a
Đoàn Thanh Dũng <dev.dung310105@gmail.com>
2023-12-19 22:52:33 +0700

    views count

---
+++ Application.Web.Database/DTOs/ResponseModels/ChartResponseModels/TotalViewsResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Application.Web.Database.DTOs.ResponseModels.ChartResponseModels
+{
+	public class TotalViewsResponseModel
+	{
+		[JsonPropertyName("totalViews")]
+		public decimal TotalViews { get; set; }
+
+		[JsonPropertyName("percentageIncreaseByLastWeek")]
+		public decimal PercentageIncreaseByLastWeek { get; set; }
+	}
+}
